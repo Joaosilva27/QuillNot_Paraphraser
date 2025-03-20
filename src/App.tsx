d001ffe254d2feed47ecd001a50f3a954d67eeef
João Silva <joaosilva7875@gmail.com>
2025-03-20 21:04:05 +0100

    tailwind init & css cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>paraphraser</h1>
+      <h1 className="text-lg">
+        Paraphraser tool built by{" "}
+        <a
+          className="text-orange-300"
+          href="https://www.joaoportfolio.com/"
+          target="_blank"
+        >
+          João Silva
+        </a>
+      </h1>
     </div>
   );
 }
