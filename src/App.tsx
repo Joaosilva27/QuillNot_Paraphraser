d4e8f20ee5fe170b4b0738a5c70bf6b6814ac411
João Silva <joaosilva7875@gmail.com>
2025-03-20 21:24:36 +0100

    working simple paraphrasing demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function App() {
+  const genAI = new GoogleGenerativeAI(
+    "AIzaSyAThR2xsb5E_ra5OfeWhqsBy3wiJZch-so"
+  );
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const [prompt, setPrompt] = useState<string>(
+    "I like burguers but rn im eating a pastel de nata wow!!"
+  );
+  const [promptResult, setPromptResult] = useState<any>("");
+
+  const onParaphrase = () => {
+    const getParaphrasingData = async () => {
+      try {
+        const intructionsForAi = "Paraphrase this text: ";
+        const newPrompt = intructionsForAi + prompt;
+        setPrompt(newPrompt);
+        console.log(newPrompt);
+
+        const result = await model.generateContent(newPrompt);
+        setPromptResult(result.response.text());
+        console.log(result.response.text());
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getParaphrasingData();
+  };
+
   return (
     <div>
       <h1 className="text-lg">
@@ -13,6 +43,10 @@ function App() {
           João Silva
         </a>
       </h1>
+      <div className="flex flex-col">
+        <button onClick={onParaphrase}>Paraphrase</button>
+        {promptResult}
+      </div>
     </div>
   );
 }
